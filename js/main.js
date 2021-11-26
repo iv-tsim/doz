daea4895ea36b381363c186d3b1dbf37587ddbac
Ivan <iv-tsim@yandex.ru>
2021-11-26 11:15:59 +0300

    t

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    let fallerySlider = new Swiper('.gallery-slider', {
 
+        slidesPerGroup: 1,
+        slidesPerView: 6,
+        spaceBetween: 30,
+        observer: true,
+        observeParents: true,
+        touchEventsTarget: 'wrapper',
+        navigation: {
+            prevEl: '.slider-arrow.gallery-arrow.gallery-arrow__left',
+            nextEl: '.slider-arrow.gallery-arrow.gallery-arrow__right',
+        }
+    });
 
 });
