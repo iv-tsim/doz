297ad04ad32844a2a2350cff1bf144f99f8f6f0d
Ivan <iv-tsim@yandex.ru>
2021-11-26 13:51:51 +0300

    g

--- js/main.js
+++ js/main.js
@@ -33,4 +33,41 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    if (document.querySelector('div#map')) {
+        ymaps.ready(function () {
+            var myMap = new ymaps.Map('map', {
+                center: [59.955885564123555,40.18514599999996],
+                zoom: 15
+            });
+            
+            var myPlacemark = new ymaps.Placemark([59.955885564123555,40.18514599999996], {
+                hintContent: 'Харовск, ул. Пустораменская, д. 50, офис 11',
+                balloonContent: 'Харовск, ул. Пустораменская, д. 50, офис 11'
+            },
+            {
+                preset: 'islands#redIcon',
+                iconLayout: 'default#image',
+                iconImageHref: '../img/general/mark.svg',
+                iconImageSize: [35, 49],
+                iconImageOffset: [-19, -52]
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+
+            myMap.controls
+                .remove('geolocationControl')
+                .remove('fullscreenControl')
+                .remove('typeSelector')
+                .remove('searchControl')
+                .remove('trafficControl')
+                .remove('zoomControl')
+                .remove('rulerControl');
+
+            myMap.behaviors.disable([
+                'scrollZoom',
+                'dblClickZoom'
+            ]);
+        });
+    }
 });
