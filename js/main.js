868f76b8871cbfd494d2d5afd07c5ace80822fa3
Ivan <iv-tsim@yandex.ru>
2021-11-26 12:32:11 +0300

    y

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function () {
+    $('input[type=tel]').mask('+7 (999) 999-99-99', {
+        completed: function () {
+            this[0].dispatchEvent(new window.Event('change', { bubbles: true }));
+        },
+    });
+});
 
+document.addEventListener('DOMContentLoaded', function () {
     let fallerySlider = new Swiper('.gallery-slider', {
-
         slidesPerGroup: 1,
         slidesPerView: 6,
         spaceBetween: 30,
@@ -11,7 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
         navigation: {
             prevEl: '.slider-arrow.gallery-arrow.gallery-arrow__left',
             nextEl: '.slider-arrow.gallery-arrow.gallery-arrow__right',
-        }
+        },
     });
 
+    document.addEventListener('change', function (event) {
+        const target = event.target;
+
+        if (target.matches('.input') && !target.classList.contains('hero-main__top-date__input')) {
+            const parent = target.closest('.input-wrapper');
+
+            if (target.value === '') {
+                parent.classList.remove('active');
+            } else {
+                parent.classList.add('active');
+            }
+        }
+    });
 });
